51e3dce8aab123dbf142dad60dc2434ba5ad2099
Timir699 <kazi.monirul699@gmail.com>
2023-06-14 23:10:54 +0600

    test

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import cors from 'cors'
-import express, { Application, Request, Response } from 'express'
+import express, { Application, Request } from 'express'
 const app: Application = express()
 
 app.use(cors())
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }))
 
 // testing
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req: Request, res: any) => {
   res.send('Setup Complete')
 })
 
