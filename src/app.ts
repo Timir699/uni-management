4045adeb691282c83a97bc85370a9972e3a8524c
Timir699 <kazi.monirul699@gmail.com>
2023-06-16 15:11:53 +0600

    correction

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import cors from 'cors'
 import express, { Application, Request, Response } from 'express'
+import usersRouter from './app/modules/users/users.routes'
+
 const app: Application = express()
 
 app.use(cors())
@@ -8,9 +10,18 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// application routes
+
+app.use('/api/v1/users/', usersRouter)
+
 // testing
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', async (req: Request, res: Response) => {
+  // await usersService.createUser({
+  //   id: '999',
+  //   password: '1234',
+  //   role: 'student',
+  // })
   res.send('Setup Complete')
 })
 
